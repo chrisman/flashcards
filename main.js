414fe8e0ffb9abc7f470b5d790b5deb68a8e9786
Christopher Brown <christopher.p.brown@gmail.com>
2015-11-14 12:44:18 -0700

    mouseover mouseout

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 $(document).ready(function(){
-  // var cards = jsyaml.load('greeting: hello\nname: world');
   var cards = jsyaml.load('greeting: hello\nname: world');
   $('div').text(cards['greeting']);
+  $('div').on('mouseover', function(){
+    $(this).text(cards['name']);
+  });
+  $('div').on('mouseout', function(){
+    $(this).text(cards['greeting']);
+  });
 });
