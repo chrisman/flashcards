d4b48397bf832acb15d446c5ee0ca37f3d02e636
Christopher Brown <christopher.p.brown@gmail.com>
2015-11-14 12:46:44 -0700

    todo: next steps

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
+/*
+TODO:
+1. how to load a local file?
+2. change mouseover/mouseout to 'on click = toggle..'?
+*/
+
 $(document).ready(function(){
   var cards = jsyaml.load('greeting: hello\nname: world');
   $('div').text(cards['greeting']);
+
   $('div').on('mouseover', function(){
     $(this).text(cards['name']);
   });
